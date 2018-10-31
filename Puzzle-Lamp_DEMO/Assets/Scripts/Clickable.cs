9ac68a3a6fc5d09b87218422d040617761996f9d
Kat <kate.rizhakova@gmail.com>
2018-10-31 13:01:02 +0100

    Demo Level1

---
+++ Puzzle-Lamp_DEMO/Assets/Scripts/Clickable.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour {
+	int layerMask;
+	public bool isInside = false;
+	public enum Colors {
+		A, B, C
+	};
+	public Colors myColor;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+		layerMask = LayerMask.GetMask("Clickable");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		if (Physics.Raycast (ray, out hit, 1000.0f, layerMask) ) {
+			if (Input.GetMouseButtonDown(0) && !isInside) {
+				Debug.Log("box should move inside the cube");
+				transform.Translate(1f, 0, 0);
+				isInside = true;
+			}
+			else if (Input.GetMouseButtonDown(0) && isInside)
+			{
+				Debug.Log("box should move outside the cube");
+				transform.Translate(-1f, 0, 0);
+				isInside = false;
+			}		
+		
+		}
+
+	}
+
+//	void OnMouseOver(){
+//		RaycastHit hit;
+//		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//
+//		if (Physics.Raycast (ray, out hit, 1000.0f, layerMask) ) {
+//			if (Input.GetMouseButton(0) && !isInside) {
+//				Debug.Log("box should move inside the cube");
+//				transform.Translate(1f, 0, 0);
+//				isInside = true;
+//			}
+//			else if (Input.GetMouseButton(0) && isInside)
+//			{
+//				Debug.Log("box should move outside the cube");
+//				transform.Translate(-1f, 0, 0);
+//				isInside = false;
+//			}		
+//
+//		}
+//
+//	}
+}
---
+++ Puzzle-Lamp_DEMO/Assets/Scripts/Projection.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projection : MonoBehaviour {
+
+	int filterNumber;
+	GameObject[] filters;
+	bool[] filtersValue;
+	Clickable script;
+	public string text = "";
+
+
+	// Use this for initialization
+	void Start () {
+
+		filters = GameObject.FindGameObjectsWithTag ("Filter");
+		filterNumber = filters.Length;
+		filtersValue = new bool[filterNumber];
+
+		text = GetComponent<TextMesh> ().text;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text = "";
+		for (int i = 0; i < filters.Length; i++) {
+
+			script = filters [i].GetComponent<Clickable> ();
+			filtersValue [i] = script.isInside; 
+		}
+		
+		for (int i = 0; i < filtersValue.Length; i++) {
+			if (filtersValue [i]) {
+				script = filters [i].GetComponent<Clickable> ();
+				if (script.myColor == Clickable.Colors.A)
+					text += "A";
+				if (script.myColor == Clickable.Colors.B )
+					text += "B";
+				if (script.myColor == Clickable.Colors.C )
+					text += "B";	
+
+			}
+		
+		}
+	}
+}
